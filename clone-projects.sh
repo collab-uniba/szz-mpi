35694545ff60815bddceee61879ad70cc174a528
Gianluca Colaianni <fast_luca@hotmail.it>
2019-02-18 09:16:12 +0100

    Updated .sh commands

--- clone-projects.sh
+++ clone-projects.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export PYTHONPATH=.:$PYTHONPATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 echo "Cloning projects from $1 into $2"
 if [ -z "$3" ]; then
     python githubutils/clone_projects.py --from=$1 --to=$2
--- szz.sh
+++ szz.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 export PYTHONPATH=.:$PYTHONPATH
-/usr/lib64/openmpi/bin/mpiexec -n $1 python szz/SzzAlgorithm.py --repo=$2 --issues=$3 --output=$4
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+if [ -z "$4" ]; then
+    python szz/SzzAlgorithm.py --repo=$1 --issues=$2 --output=$3
+elif [ -z "$5" ]; then
+    mpiexec -n $1 python szz/SzzAlgorithm.py --repo=$2 --issues=$3 --output=$4
+else
+    export PATH=$PATH:$1
+    mpiexec -n $2 python szz/SzzAlgorithm.py --repo=$3 --issues=$4 --output=$5
+fi
